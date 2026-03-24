8309f22f87e667f6aa10c925ba13b49f94747c4e
Akshaya M <akshayamaruthu2005@gmail.com>
2026-03-24 20:04:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,39 +9,30 @@
  * }
  */
 class Solution {
-    
-    public ListNode mergeKLists(ListNode[] lists) {
-        if (lists == null || lists.length == 0) return null;
-
-        ListNode result = lists[0];
-
-        for (int i = 1; i < lists.length; i++) {
-            result = mergeTwoLists(result, lists[i]);
+    public ListNode mergeKLists(ListNode[] lists){
+        if (lists==null||lists.length==0) 
+            return null;
+        ListNode result=lists[0];
+        for (int i=1;i<lists.length;i++){
+            result=mergeTwoLists(result,lists[i]);
         }
-
         return result;
     }
-
-    // Merge 2 sorted lists
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                tail.next = l1;
-                l1 = l1.next;
-            } else {
-                tail.next = l2;
-                l2 = l2.next;
+    public ListNode mergeTwoLists(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode tail=dummy;
+        while (l1!=null &&l2!=null){
+            if (l1.val<l2.val){
+                tail.next=l1;
+                l1=l1.next;
+            } else{
+                tail.next=l2;
+                l2=l2.next;
             }
-            tail = tail.next;
+            tail=tail.next;
         }
-
-        // remaining nodes
-        if (l1 != null) tail.next = l1;
-        if (l2 != null) tail.next = l2;
-
+        if (l1!=null) tail.next = l1;
+        if (l2!=null) tail.next = l2;
         return dummy.next;
     }
 }
